13cc0190e3919c801bebd7fb3d4cb89768fe64ec
lucasvnunes-lgtm <lucas.v.nunes@aluno.senai.br>
2025-10-21 16:54:17 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+
 import './JogoDaVelha.css';
+import React, { useState } from 'react';
 
 
 function JogoDaVelha() {
 
-  const empytBoard = Array(9).fill("")
-  const [board, setBoard] = useState([""])
+  const empytBoard = Array(9).fill("");
+  const [board, setBoard] = useState(empytBoard);
+  const handleCellClick = (index) => {
+// o item é o nome do vetor. O index é a posição dele
+  setBoard([board.map (item, itemIndex) => ]);
+  }
   return (
 
     <main>  
       <h1 className='title'> Jogo Da Velha de Lucas </h1>   
 
       <div className='board'>
-      <div className='cell'>.</div>
-      
+        {board.map((item,index) =>
+        ( <div key={index}
+          className={`cell ${item}`}
+          onClick={() => handleCellClick(index)}>
+            
+            {item}</div> )
+        
+        )}
       </div>
    </main>
   );
